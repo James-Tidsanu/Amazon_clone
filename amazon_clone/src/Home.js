819cec02563cf04e80c2c92c00d368ca2494d4c0
Tidsanu Nampradid <Jtn_james@hotmail.com>
2023-07-05 20:37:18 +0100

    homepage

--- amazon_clone/src/Home.js
+++ amazon_clone/src/Home.js
@@ -13,18 +13,48 @@ function Home() {
          />
 
          <div className='home__row'>
-          <Product/>
-          <Product/>
+          <Product
+          id='0023881'
+          title='Kindle Paperwhite | 16 GB, now with a 6.8" display and adjustable warm light | With ads | Black.'
+          price={149.99}
+          image='https://m.media-amazon.com/images/I/51Knn1dABuL._AC_UY436_FMwebp_QL65_.jpg'
+          rating={5} />
+          <Product
+           id='0777111'
+           title='Smart Watch, 1.69" Touch Fitness Watch for Men Women, Fitness Tracker Smartwatch with Heart Rate Sleep Monitor Step Counter.'
+           price={22.78}
+           image='https://m.media-amazon.com/images/I/61W-xQ95sWL._AC_SX679_.jpg'
+           rating={4} />
          </div>
 
          <div className='home__row'>
-          {/* product */}
-          {/* product */}
-          {/* product */}
+         <Product
+           id='0000291'
+           title='Massage Gun, RENPHO Massage Gun Deep Tissue with Heat Massage Head, Up to 3200RPM Muscle Massager with Type-C Charging.'
+           price={104.99}
+           image='https://m.media-amazon.com/images/I/71OK1NDPC2L._AC_SX679_.jpg'
+           rating={4} />
+         <Product
+           id='0929111'
+           title='EMtronics EMAFDD9LSL Dual Air Fryer Extra Large Family Size Double XL 9 Litre with 8 Pre-Set Menus for Oil Free & Low Fat Healthy Cooking.'
+           price={109.99}
+           image='https://m.media-amazon.com/images/I/71PRWIUlp7L._AC_SX679_.jpg'
+           rating={5} />
+         <Product
+           id='0000044'
+           title='IPL Hair Removal Device, 9 Energy Level,3 Functions HR/SC/RA, 999,900 Light Pulses,Permanent Painless Laser Hair Removal Long Lasting for whole Body.'
+           price={88.82}
+           image='https://m.media-amazon.com/images/I/61fWiDEyF9L._AC_SX679_.jpg'
+           rating={5} />
          </div>
 
          <div className='home__row'>
-          {/* product */}
+         <Product
+           id='0778399'
+           title='Philips Lumea IPL Hair Removal 7000 Series - Hair Removal Device with Satin Compact Pen Trimmer, 2 Attachments for Body and Face, Corded Use (Model BRI921/00).'
+           price={269}
+           image='https://m.media-amazon.com/images/I/61cwp22ST8L._AC_SX679_.jpg'
+           rating={5} />
          </div>
 
       </div>
--- amazon_clone/src/Product.js
+++ amazon_clone/src/Product.js
@@ -2,23 +2,27 @@ import React from 'react'
 import './Product.css';
 
 
-function Product() {
+function Product({ title, image, price, rating }) {
   return (
     <div
     className='product'>
       <div
       className='product__info'>
-        <p>Top Deal</p>
+        <p>{title}</p>
         <p className='product__price'>
             <small>£</small>
-            <strong>19.99</strong>
+            <strong>{price}</strong>
         </p>
         <div className='product__rating'>
-          <p>⭐⭐⭐⭐</p>
+          {Array(rating)
+          .fill()
+          .map((_,i) => (
+           <p>⭐</p>
+          ))}
         </div>
       </div>
       <img
-         src='https://m.media-amazon.com/images/I/41I7QQ1vw-L._AC_SY460_.jpg'
+         src={image}
          atl=''
          />
 
