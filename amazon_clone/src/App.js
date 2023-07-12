1e0eb31be483c60bf2456532ea5bf926e28f91d0
Tidsanu Nampradid <Jtn_james@hotmail.com>
2023-07-12 14:33:51 +0100

    2page

--- amazon_clone/src/App.js
+++ amazon_clone/src/App.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 
 function App() {
   return (
     // BEM
-    <div className="App">
-      <Header />
-      <Home/>
-    </div>
+    // <BrowserRouter>
+    //   <div className="App">
+    //     <Routes>
+    //     <Route path="/hello" element={<h1>hello</h1>} />
+    //       </Routes>
+    //   </div>
+    // </BrowserRouter>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path='/checkout' element={<Header />}>
+          </Route>
+        </Routes>
+        <Routes>
+          <Route path='/' element={<Home><Header /></Home>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
