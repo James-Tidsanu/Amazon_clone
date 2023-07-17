9993f09fd470e522fcd3da636c8c191c628a0159
Tidsanu Nampradid <Jtn_james@hotmail.com>
2023-07-17 23:59:38 +0100

    fix routes

--- amazon_clone/src/App.js
+++ amazon_clone/src/App.js
@@ -2,34 +2,32 @@ import React, { Fragment } from 'react';
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import Checkout from './Checkout'
+//import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import {
   BrowserRouter,
+  Switch,
   Routes,
   Route,
+  Link,
+  useRouteMatch
 } from "react-router-dom";
-
 
 function App() {
   return (
     // BEM
-    // <BrowserRouter>
-    //   <div className="App">
-    //     <Routes>
-    //     <Route path="/hello" element={<h1>hello</h1>} />
-    //       </Routes>
-    //   </div>
-    // </BrowserRouter>
+
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path='/checkout' element={<Header />}>
-          </Route>
-        </Routes>
-        <Routes>
-          <Route path='/' element={<Home><Header /></Home>} />
+          <Route path='/checkout' element={<><Header/><Checkout/></>} />
+          <Route path='/' element={<><Header/><Home/></>} />
+          <Route path='/orders' element={<h1>Here are your orders</h1>} />
+          <Route path='/sign-in' element={<h1>Enter your passsword</h1>} />
         </Routes>
       </div>
     </BrowserRouter>
+
   );
 }
 
---
+++ amazon_clone/src/Checkout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Checkout.css';
+
+function Checkout() {
+  return (
+   <div className='checkout'>
+    <div className='checkout__left'>
+      <img
+       className='checkout__ad' src='https://m.media-amazon.com/images/G/02/UK_CCMP/AIS/577-HL-LX-Maple-770x70-1._CB607807473_.jpg'
+       alt=''
+      />
+    </div>
+   </div>
+  );
+}
+export default Checkout;
