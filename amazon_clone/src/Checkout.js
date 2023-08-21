132e2bd6a644599956d4531177f08e8931ebab67
Tidsanu Nampradid <Jtn_james@hotmail.com>
2023-08-21 23:23:15 +0100

    link

--- amazon_clone/src/Checkout.js
+++ amazon_clone/src/Checkout.js
@@ -9,6 +9,13 @@ function Checkout() {
        className='checkout__ad' src='https://m.media-amazon.com/images/G/02/UK_CCMP/AIS/577-HL-LX-Maple-770x70-1._CB607807473_.jpg'
        alt=''
       />
+      <div>
+        <h2 className='checkout__title'>Your Shopping Basket</h2>
+      </div>
+
+      <div className='checkout__right'>
+        <h2></h2>
+      </div>
     </div>
    </div>
   );
--- amazon_clone/src/Header.js
+++ amazon_clone/src/Header.js
@@ -2,14 +2,17 @@ import React from 'react';
 import './Header.css';
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <div className='Header'>
+      <Link to='/'>
       <img
         className='header__logo'
         src='https://wildfiresocial.com/wp-content/uploads/2019/01/amazon-logo-white._cb1509666198_.png'
         />
+      </Link>
 
       <div className='header__search'>
         <input className='header__searchInput'
@@ -39,11 +42,12 @@ function Header() {
           className='header__optionLineTwo'>Prime</span>
         </div>
 
-      <div className='header__optionBasket'>
-        <ShoppingBasketIcon />
-        <span className='header_optionLineTwo header_basketCount'>0</span>
-        </div>
-
+        <Link to='/checkout'>
+          <div className='header__optionBasket'>
+            <ShoppingBasketIcon />
+            <span className='header_optionLineTwo header_basketCount'>0</span>
+          </div>
+          </Link>
       </div>
     </div>
   );
