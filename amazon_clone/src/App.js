69affaa7aa1507615cf417f5c591bb12fd76470f
Tidsanu Nampradid <Jtn_james@hotmail.com>
2024-02-05 16:40:59 +0000

    user on firebase

--- amazon_clone/src/App.js
+++ amazon_clone/src/App.js
@@ -2,36 +2,44 @@ import React, { useEffect } from 'react';
 import './App.css';
 import Header from './Header';
 import Home from './Home';
-import Checkout from './Checkout'
-import {
-  BrowserRouter,
-  Switch,
-  Routes,
-  Route,
-  Link,
-  useRouteMatch
-} from "react-router-dom";
+import Checkout from './Checkout';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Login';
+import { auth } from './firebase';
+import { useStateValue } from './StateProvider';
 
 function App() {
+  const [{}, dispatch] = useStateValue();
+
   useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      console.log('The User is >>>', authUser);
+      if (authUser) {
+        dispatch({
+          type: 'SET_USER',
+          user: authUser,
+        });
+      } else {
+        dispatch({
+          type: 'SET_USER',
+          user: null,
+        });
+      }
+    });
+  }, [dispatch]); // Added dependencies array
 
-  }, [])
   return (
-    // BEM
-
-    <BrowserRouter>
+    <Router>
       <div className="App">
         <Routes>
-          <Route path='/login' element={<><Header/><Login/></>} />
-          <Route path='/checkout' element={<><Header/><Checkout/></>} />
-          <Route path='/' element={<><Header/><Home/></>} />
-          <Route path='/orders' element={<h1>Here are your orders</h1>} />
-          <Route path='/sign-in' element={<h1>Enter your passsword</h1>} />
+          <Route path="/login" element={<><Header/><Login/></>} />
+          <Route path="/checkout" element={<><Header/><Checkout/></>} />
+          <Route path="/" element={<><Header/><Home/></>} />
+          <Route path="/orders" element={<h1>Here are your orders</h1>} />
+          <Route path="/sign-in" element={<h1>Enter your password</h1>} />
         </Routes>
       </div>
-    </BrowserRouter>
-
+    </Router>
   );
 }
 
